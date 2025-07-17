905699295ad7e5575224aeccf87092bf2d523cbb
Volodymyr Khomaiko <volodymyr.khomaiko@student.tuke.sk>
2025-07-17 22:51:40 +0200

    0

--- lesson 4/lesson 4.2.py
+++ lesson 4/lesson 4.2.py
@@ -1,11 +1,10 @@
 lst = [0, 1, 7, 2, 4, 8]
 
-if not lst:
-    result = 0
-else:
-    sum_even_index = 0
-    for i in range(0, len(lst), 2):
-        sum_even_index += lst[i]
+result = 0
+sum_even_index = 0
+
+for i in range(0, len(lst), 2):
+    sum_even_index += lst[i]
     result = sum_even_index * lst[-1]
 
 print(result)
