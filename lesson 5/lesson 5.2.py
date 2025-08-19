5a12679201c2b7be0cc9de006d6c1ede22577133
Volodymyr Khomaiko <volodymyr.khomaiko@student.tuke.sk>
2025-08-19 22:50:50 +0200

    0,00

---
+++ lesson 5/lesson 5.2.py
@@ -0,0 +1,24 @@
+while True:
+
+    nd = float(input("Enter first number: "))
+    r = float(input("Enter second number: "))
+
+    action = input("Enter action (+, -, *, /): ")
+
+    if action == "+":
+        print(nd + r)
+
+    elif action == "-":
+        print(nd - r)
+
+    elif action == "*":
+        print(nd * r)
+
+    elif action == "/":
+        print(nd / r)
+        if r == 0:
+            print("Error! You cannot divide by zero!")
+
+    cnt = input("yes/y:").lower()
+    if cnt not in ("yes", "y"):
+            break
