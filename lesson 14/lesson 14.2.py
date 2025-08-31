123fb43b82a7bde6638e8034f3680c828c13d30b
Volodymyr Khomaiko <volodymyr.khomaiko@student.tuke.sk>
2025-08-31 21:33:12 +0200

    14.2

---
+++ lesson 14/lesson 14.2.py
@@ -0,0 +1,51 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.gender}, {self.age}"
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.record_book})"
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __hash__(self):
+        return hash(str(self))
+
+class GroupFullException(Exception):
+    pass
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupFullException("Группа полна! Нельзя добавить больше 10 студентов.")
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(s) for s in self.group)
+        return f'Number: {self.number}\n{all_students}'
